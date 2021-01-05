7a0c0574ec4e09a42b33b14e93fd75fec1535bda
Mark <markmeldrum70@gmail.com>
2021-01-05 19:23:55 +0000

    First commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+    private int intNum1;
+    private int intNum2;
+    private double dblNum1;
+    private double dblNum2;
+
+    public Calculator(int intNum1, int num2){
+        this.int1 = int1;
+        this.int2 = int2;
+    }
+
+
+    /*
+     Create a Calculator class. This should have functions for Add, Subtract, Multiply and Divide.
+     Your methods should take in two ints to perform the calculations on (except the Divide method.
+     This should take two doubles as arguments)
+    */
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,15 @@
+public class CalculatorTest {
+
+    public class CalculatorTest{
+
+        Calculator calculator;
+
+
+    }
+    /*
+     Create a Calculator class. This should have functions for Add, Subtract, Multiply and Divide.
+     Your methods should take in two ints to perform the calculations on (except the Divide method.
+     This should take two doubles as arguments)
+    */
+
+}
